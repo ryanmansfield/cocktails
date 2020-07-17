a7ccb1e2f575c0141115375479a31f2f25d395cf
Ryan Mansfield <ryanmansfield86@gmail.com>
2020-07-17 12:02:00 -0500

    pg search added

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -4,4 +4,11 @@ class Cocktail < ApplicationRecord
   has_many :ratings
   validates :name, uniqueness: true, presence: true, allow_blank: false
   has_one_attached :photo
+
+  include PgSearch::Model
+  pg_search_scope :search_by_name,
+    against: [ :name ],
+    using: {
+      tsearch: { prefix: true }
+    }
 end
