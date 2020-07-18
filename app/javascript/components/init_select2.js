92702363a61bbd9fccd1e532f47c0689ed5b0b55
Ryan Mansfield <ryanmansfield86@gmail.com>
2020-07-18 16:49:46 -0500

    select2-bootstrap-theme added

--- app/javascript/components/init_select2.js
+++ app/javascript/components/init_select2.js
@@ -1,12 +1,14 @@
 import select2 from "select2";
 
 import 'select2/dist/css/select2.css';
+import 'select2-bootstrap-theme/dist/select2-bootstrap.css'
+
 
 const initSelect2 = () => {
   $(document).ready(function() {
     $('.select2-dropdown').select2({
       theme: "bootstrap",
-      dropdownAutoWidth: 'true',
+      // dropdownAutoWidth: 'true',
       placeholder: "Select ingredient",
       allowClear: true,
       width: '200px',
