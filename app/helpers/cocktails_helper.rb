ea8e155913acd8b0a56d974189a82d87f6e4c06b
Ryan Mansfield <ryanmansfield86@gmail.com>
2020-07-19 02:46:55 -0500

    cocktail rating rendered as star

--- app/helpers/cocktails_helper.rb
+++ app/helpers/cocktails_helper.rb
@@ -1,2 +1,9 @@
 module CocktailsHelper
+  def render_stars(value)
+    output = ''
+    if (1..5).include?(value.to_i)
+      value.to_i.times { output += '<i class="fas fa-star"></i>'}
+    end
+    output
+  end
 end
